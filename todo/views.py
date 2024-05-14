7d6ea22155cf72c285dc76bf2ae1c7b8743790d9
jpythink <jaynilrbpatel@gmail.com>
2024-05-14 19:20:15 +0530

    mark as done functionality

--- todo/views.py
+++ todo/views.py
@@ -6,4 +6,12 @@ from .models import Task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.is_completed = True
+    task.save()
+
     return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,10 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
+    complated_tasks = Task.objects.filter(is_completed = True)
     
     context = {
-        'tasks':tasks
+        'tasks':tasks,
+        'complated_tasks': complated_tasks
     }
     return render(request, "home.html", context)
